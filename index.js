8007d2837c17d7f598d0508c31a56bb2e409ca36
Yashad <yakwinter@gmail.com>
2025-04-02 13:01:31 +0300

    Hello World

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Запускаем таймер");
+for (let i = 5; i >= 0; i--) {
+    if (i === 0) {
+        alert(i);
+        console.log("Обратный отсчет закончен");
+    } else {
+        alert(i);
+    }
+}
---
+++ prompt/index.js
@@ -0,0 +1,20 @@
+let sum = Number(prompt("Сколько будет 2 + 2?"));
+if (sum === 4) {
+    alert("Правильно!")
+} else {
+    alert("Не правильно, ответ 4!")
+}
+
+let rad = Number(prompt("Чему равен корень из 9?"));
+if (rad === 3) {
+    alert("Правильно!")
+} else {
+    alert("Не правильно, ответ 3!")
+}
+
+let degree = Number(prompt("Сколько будет 2 в 5-й степени?"));
+if (degree === 32) {
+    alert("Правильно!")
+} else {
+    alert("Не правильно, ответ 32!")
+}
